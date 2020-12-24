d6d7342caa246878d8184e0ba84c64ff5f9c2c6e
Jacob Hampton <jhampton792@gmail.com>
2020-12-24 21:43:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,17 +3,16 @@
 
 def key_for_min_value(name_hash)
   
-  keys = []
-  values = []
-  index = 0
+  smallest_key = name_hash.first[0]
+  smallest_value = name_hash.first[1]
   
   name_hash.each do | key, value |
-    keys << key
-    values << value
+    if value < smallest_value
+      smallest_value = value
+      smallest_key = key
+    end
   end
   
-  smallest_value = values[0]
-  
-  values.each do 
+  smallest_key
 
 end
